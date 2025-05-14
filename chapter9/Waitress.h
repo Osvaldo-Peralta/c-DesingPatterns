8e778efb7ab04abff127e3c0a360ff603805862f
Osvaldo Peralta <osvaldo.peralta@dye-industriales.com>
2025-05-14 11:53:02 -0600

    Implementamos la iteracion generica usando MenuTraits

--- chapter9/Waitress.h
+++ chapter9/Waitress.h
@@ -1,20 +1,49 @@
-#ifndef WAITRESS_H
-#define WAITRESS_H
+// Waitress.h
+#include <iostream>
+#include "MenuTraits.h"
 
-#include "PancakeHouseMenu.h"
-#include "DinerMenu.h"
-#include "Iterator.h"
-
+template <typename PancakeIter, typename DinerIter, typename CafeIter>
 class Waitress
 {
 private:
-    PancakeHouseMenu &pancakeHouseMenu;
-    DinerMenu &dinerMenu;
-    void printMenu(Iterator *iterator);
+    PancakeIter pancakeBegin;
+    PancakeIter pancakeEnd;
+    DinerIter dinerBegin;
+    DinerIter dinerEnd;
+    CafeIter cafeBegin;
+    CafeIter cafeEnd;
+
+    // Función genérica para imprimir cualquier menú
+    template <typename Iter>
+    void printMenu(Iter begin, Iter end)
+    {
+        for (auto it = begin; it != end; ++it)
+        {
+            // Usamos el trait con el tipo correcto
+            const MenuItem &item = MenuTraits<decltype(*it)>::getItem(*it);
+            cout << item.getName() << ", $" << item.getPrecio()
+                 << " -- " << item.getDescription()
+                 << endl;
+        }
+    }
 
 public:
-    Waitress(PancakeHouseMenu &pancakeHouseMenu, DinerMenu &dinerMenu);
-    void printMenu();
-};
+    Waitress(PancakeIter pBegin, PancakeIter pEnd,
+             DinerIter dBegin, DinerIter dEnd,
+             CafeIter cBegin = {}, CafeIter cEnd = {})
+        : pancakeBegin(pBegin), pancakeEnd(pEnd),
+          dinerBegin(dBegin), dinerEnd(dEnd),
+          cafeBegin(cBegin), cafeEnd(cEnd) {}
 
-#endif
+    void printAllMenus()
+    {
+        cout << "MENU\n----\nBREAKFAST" << endl;
+        printMenu(pancakeBegin, pancakeEnd);
+
+        cout << "\nLUNCH" << endl;
+        printMenu(dinerBegin, dinerEnd);
+
+        cout << "\nDINNER" << endl;
+        printMenu(cafeBegin, cafeEnd);
+    }
+};
